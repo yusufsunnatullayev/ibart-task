9c5ad3a0394e5036e7e8238f77f8fce4187bbd6a
Sayfullayev1407 <ageny61@gmail.com>
2025-08-30 18:16:14 +0500

    about-page-done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+
 import React from "react";
+import HowItWorks from "@/components/elements/HowItWorks";
+import Benefits from "@/components/elements/Benefits";
+import Testimonials from "@/components/elements/Testimonials";
+import JoinUs from "@/components/elements/JoinUs";
+import FAQ from "@/components/elements/FAQ";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen">
+      <main>
+        <HowItWorks/>
+        <Benefits/>
+        <Testimonials/>
+        <JoinUs/>
+        <FAQ/>
+      </main>
+    </div>
+  );
 };
 
 export default About;
