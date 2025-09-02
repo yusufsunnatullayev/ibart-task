d37c6e6758e2915a0fa590f0a99f95371cde6d37
Yusuf Sunnatullayev <yusuffdeveloper@gmail.com>
2025-09-02 13:17:25 +0500

    fix: height of the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="h-full flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
